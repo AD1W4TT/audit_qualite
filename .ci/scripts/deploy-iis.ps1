362174c105bd6dae3b299784b934529e235c850d
Louis THIBAULT <louis.thibault@ADIWATT.COM>
2026-02-24 10:02:52 +0100

    ci: handle robocopy warning exit codes

--- .ci/scripts/deploy-iis.ps1
+++ .ci/scripts/deploy-iis.ps1
@@ -25,6 +25,8 @@ function Invoke-Robocopy {
   & robocopy $From $To /MIR /R:2 /W:2 /NFL /NDL /NP
   $code = $LASTEXITCODE
   if ($code -ge 8) { throw "Robocopy failed with exit code $code" }
+  if ($code -ge 1) { Write-Host "Robocopy completed with warnings/code $code" }
+  $global:LASTEXITCODE = 0
 }
 
 if (Test-Path $Target) {
@@ -47,3 +49,4 @@ catch {
 finally {
   if (Test-Path $appOffline) { Remove-Item $appOffline -Force }
 }
+
